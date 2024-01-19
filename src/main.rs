5b685cdf13d15eaa5c47daa18b6fbb38295dc5fb
Space Nerd <71553327+SpaceNerde@users.noreply.github.com>
2024-01-19 16:20:22 +0100

    basic setup

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use bevy::prelude::*;
+
+fn main() {
+    let mut app = App::new();
+    app.insert_resource(Msaa::Off)
+        .insert_resource(ClearColor(Color::rgb(0.4, 0.4, 0.4)))
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "RSIN - Rusty Simulated Network".to_string(),
+                // Bind to canvas included in `index.html`
+                canvas: Some("#bevy".to_owned()),
+                // The canvas size is constrained in index.html and build/web/styles.css
+                fit_canvas_to_parent: true,
+                // Tells wasm not to override default event handling, like F5 and Ctrl+R
+                prevent_default_event_handling: false,
+                ..default()
+            }),
+            ..default()
+        }));
+    app.run();
+}
